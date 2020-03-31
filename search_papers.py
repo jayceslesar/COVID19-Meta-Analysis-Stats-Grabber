dcd5077f8a94c91a5b3ae261e5b4d82829314a43
jayceslesar <47452474+jayceslesar@users.noreply.github.com>
2020-03-31 16:20:38 -0400

    keyword updates

--- search_papers.py
+++ search_papers.py
@@ -26,26 +26,30 @@ def extract(keywords, text):
     return return_data
 
 
-# requires list of links of databases to check
+# requires database link and list of keywords to filter searches by
 # returns the text mined PDF of paper (will look into more exact data extraction)
 def search_daily(link, keywords):
     return_data = {}
     r = requests.get(link)
-    with open("data.xlsx", 'wb') as f:
+    with open("today.xlsx", 'wb') as f:
         f.write(r.content)
-    path = Path(Path.cwd() / 'data.xlsx')
+    path = Path(Path.cwd() / 'today.xlsx')
     df= pd.read_excel(path)
-    print(df.columns)
-    return
     for index, row in df.iterrows():
-       title = row['Title']
-
-    return_data[link] = extract(keywords, requests.get(link))
+       title_words = row['Title'].split()
+       print(title_words)
+       clause = False
+       if clause:
+            return_data[row['Title']] = extract(keywords, requests.get(link))
+       break
+    return
     return return_data
 
 
 link = 'https://www.cdc.gov/library/docs/covid19/ONLY_newarticles_30March2020_Excel.xlsx'
-keywords = ['corona']
-search_daily(link, keywords)
-
-# print(search_daily(links, keywords))
+keyphrases = ['Transmission rate', 'Total population', 'Latency period',
+              'Reporting rate', 'Infectious period reported', 'Infectious period unreported',
+              'Percent recovered', 'recover time', 'hospitalization rate', 'unreported hospitalization',
+              'Percentage of hospitalized who needs ICU', 'Time from hospitalization to ICU', 'Percentage of ICU patients who recover',
+              'Time from ICU admission to recovery', 'Percentage of ICU patients who die', 'Time from ICU admission to death']
+search_daily(link, keyphrases)
