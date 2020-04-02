10c464056b2489c287088def49e5fb5f3282f8b5
jayceslesar <47452474+jayceslesar@users.noreply.github.com>
2020-04-02 15:44:45 -0400

    pathfinding and keyword updates

--- Daily_Article_Sweep.py
+++ Daily_Article_Sweep.py
@@ -8,29 +8,32 @@ import urllib3
 from pathlib import Path
 from datetime import date
 import datetime
+import lxml.html
 
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 
-def find_relevant_titles(title, keywords):
+def find_relevant_titles(title, keywords, bad_keywords):
     title_words = [word.lower() for word in title.split(' ')]
-    if 'gene' in title_words:
-        return False
+    for bad in bad_keywords:
+        if bad.lower() in title_words:
+            return False
     probs = []
     for keyword in keywords:
         for title_word in title_words:
             if len(title_word) > 2:
-                if similar(title_word, keyword.lower()) > 0.65:
+                if similar(title_word, keyword.lower()) > 0.75:
                     probs.append(True)
     if len(probs) > 1:
         return True
     else:
         return False
 
-class Daily_Article_Sweep:
 
+class Daily_Article_Sweep:
+    # initializer
     def __init__(self):
         today = date.today()
         d2 = today.strftime("%B %d %Y").split()
@@ -42,30 +45,31 @@ class Daily_Article_Sweep:
         if d2[1][0] == '0':
             day = d2[1][1:]
         self.DATE_YESTERDAY = day + d2[0] + d2[2]
-        self.CDC_PAPERS = 'https://www.cdc.gov/library/docs/covid19/ONLY_newarticles_' + self.DATE + '_Excel.xlsx'
-        self.CDC_PAPERS_YESTERDAY = 'https://www.cdc.gov/library/docs/covid19/ONLY_newarticles_' + \
-            self.DATE_YESTERDAY + '_CDC.xlsx'
+        self.CDC_LINK = 'https://www.cdc.gov/library/researchguides/2019novelcoronavirus/researcharticles.html'
+        self.CDC_PAPERS = 'https://www.cdc.gov'
         self.JSON_PAPERS = 'https://connect.medrxiv.org/relate/collection_json.php?grp=181'
-        self.KEYPHRASES = ['Transmission', 'rate', 'population', 'Latency', 'period',
+        self.KEYPHRASES = ['transmission', 'rate', 'population', 'Latency', 'period', 'spread',
                            'Reporting', 'rate', 'Infectious', 'reported', 'unreported', 'hospital',
                            'Percent', 'recovered', 'recover' , 'time', 'hospitalization', 'Percentage', 'hospitalized', 'ICU',
-                           'from','patients', 'admission', 'die', 'death']
+                           'from', 'patients', 'admission', 'die', 'death', 'differential', 'equations', 'susceptible',
+                           'exposed', 'model', 'modelling', 'infected', 'risk', 'infection', 'infect']
+        self.BAD_KEYWORDS = ['gene', 'ACE2',
+                             'chloroquine', 'remdesivir', 'favipiravir', 'rhinitis']
         self.todays_matches = []
+        self.DROPPED = 0
 
         def get_cdc(self):
             matches = []
-            try:
-                r = requests.get(self.CDC_PAPERS)
-                with open("today.xlsx", 'wb') as f:
-                    f.write(r.content)
-                path = Path(Path.cwd() / 'today.xlsx')
-                df = pd.read_excel(path)
-            except:
-                r = requests.get(self.CDC_PAPERS_YESTERDAY)
-                with open("today.xlsx", 'wb') as f:
-                    f.write(r.content)
-                path = Path(Path.cwd() / 'today.xlsx')
-                df = pd.read_excel(path)
+            page = requests.get(self.CDC_LINK).text
+            tree = lxml.html.fromstring(page)
+            link = tree.xpath(
+                '/html/body/div[6]/main/div[3]/div/div[3]/div[2]/div[2]/div/div[2]/ul[1]/li[2]/a')[0]
+            rest = link.attrib['href']
+            r = requests.get(self.CDC_PAPERS + rest)
+            with open("today.xlsx", 'wb') as f:
+                f.write(r.content)
+            path = Path(Path.cwd() / 'today.xlsx')
+            df = pd.read_excel(path)
             for index, row in df.iterrows():
                 curr_match = {}
                 curr_title = row['Title']
@@ -73,13 +77,15 @@ class Daily_Article_Sweep:
                     continue
                 if curr_title[0] == '[':
                     curr_title = curr_title[1:-1]
-                if find_relevant_titles(curr_title, self.KEYPHRASES):
+                if find_relevant_titles(curr_title, self.KEYPHRASES, self.BAD_KEYWORDS):
                     curr_match["title"] = curr_title
                     if type(row['URL']) != float and 'ovid' in row['URL']:
                         curr_match["link"] = row['DOI']
                     else:
                         curr_match["link"] = row['URL']
                     self.todays_matches.append(curr_match)
+                else:
+                    self.DROPPED += 1
 
         def get_rxiv(self):
             data = requests.get(self.JSON_PAPERS).json()
@@ -87,12 +93,16 @@ class Daily_Article_Sweep:
             for paper_data in data["rels"]:
                 curr_title = paper_data["rel_title"]
                 curr_match = {}
-                if find_relevant_titles(curr_title, self.KEYPHRASES):
+                if find_relevant_titles(curr_title, self.KEYPHRASES, self.BAD_KEYWORDS):
                     curr_match["title"] = curr_title
                     curr_match["link"] = paper_data["rel_link"]
                     self.todays_matches.append(curr_match)
+                else:
+                    self.DROPPED += 1
 
         get_cdc(self)
         get_rxiv(self)
         self.todays_data_df = pd.DataFrame(self.todays_matches)
         self.todays_data_df.to_csv('daily_to_search.csv')
+        print('Filters cleared out ' + str(self.DROPPED) + ' irrelevant articles.')
+        print('Found ' + str(len(self.todays_matches)) + ' potential matches.')
